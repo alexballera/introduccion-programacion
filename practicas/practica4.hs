aebaba069ca6af9d7cb97bcb3329115470074e5e
Alex Ballera <alexander.ballera@comafi.com.ar>
2024-04-17 12:39:30 -0300

    ejercicio 13

--- practicas/practica4.hs
+++ practicas/practica4.hs
@@ -1,7 +1,5 @@
 {-
-! *****************************************************************
 ! Ejercicio 1.
-! *****************************************************************
 
 ? Implementar la función fibonacci:: Integer->Integer que devuelve el i-ésimo número de Fibonacci.
 ? Recordar que la secuencia de Fibonacci se define como:
@@ -17,14 +15,17 @@ problema fibonacci (n: Z) : Z {
 -}
 
 fibonacci :: Int -> Int
-fibonacci n | n == 0 = 0
-            | n == 1 = 1
-            | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+-- fibonacci n | n == 0 || n == 1 = n
+--             | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+
+-- Pattern Machine
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
 {-
-! *****************************************************************
 ! Ejercicio 2.
-! *****************************************************************
+
 ? Implementar una función parteEntera :: Float -> Integer
 ? según la siguiente especificación:
 problema parteEntera (x: R) : Z {
@@ -37,14 +38,13 @@ entero :: Float -> Integer
 entero = truncate
 
 parteEntera :: Float -> Integer
-parteEntera x = if fromIntegral (entero x) <= x
-                  then entero x
-                  else entero x + 1
+parteEntera x | 0 <= x && x < 1 = 0
+               | x > 1 = 1 + parteEntera (x - 1)
+               | x < 0 = (-1) + parteEntera (x + 1)
 
 {-
-! *****************************************************************
 ! Ejercicio 3.
-! *****************************************************************
+
 ? Especificar e implementar la función esDivisible :: Integer -> Integer -> Bool
 ? que dados dos números naturales determinar si el primero es divisible por el segundo.
 ? No está permitido utilizar las funciones mod ni div
@@ -59,3 +59,53 @@ esDivisible :: Integer -> Integer -> Bool
 esDivisible m n | m < n = False
                 | m == n = True
                 | m > n = esDivisible (m - n) n
+
+{-
+! Ejercicio 4.
+? Especificar e implementar la función sumaImpares :: Integer -> Integer
+? que dado n ∈ N sume los primeros n números impares.
+? Por ejemplo: sumaImpares 3 ❀ 1+3+5 ⇝ 9.
+
+problema sumaImpares (n: N) : N {
+  requiere: { n ∈ N }
+  asegura: { Sume los primeros n números impares, m = 1 + 3 + ... , resultado: m }
+}
+-}
+
+{-
+! Ejercicio 7.
+? Implementar la función todosDigitosIguales :: Integer -> Bool
+? que determina si todos los dígitos de un número natural son iguales, es decir:
+
+problema todosDigitosIguales (n: Z) : B {
+  requiere: { n > 0 }
+  asegura: { resultado = true ↔ todos los dígitos de n son iguales }
+}
+-}
+digitoUnidades :: Integer -> Integer
+digitoUnidades n = mod n 10 
+
+sacarUnidades :: Integer -> Integer
+sacarUnidades n = div n 10 
+
+{- todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n | n < 10 = True
+                      | otherwise = (digitoUnidades) -}
+
+{-
+! Ejercicio 13.
+? Especificar e implementar la siguiente función:
+
+problema sumatoris (n: Z, m: Z) : Z {
+  requiere: { n > 0 }
+  asegura: { resultado = sumatoria }
+}
+-}
+
+g :: Int -> Int -> Int
+g 0 _ = 0
+g m i = g (m - 1) i + i^m
+
+f :: Int -> Int -> Int
+f 0 _ = 0
+f n m = f (n - 1) m + g n m 
