e2f43f61e4d973562fc14a52621b7aee062d7f18
Alex Ballera <alexballera@gmail.com>
2024-05-03 14:49:33 -0300

    contarPalabras

--- practicas/practica5/ejercicio4.hs
+++ practicas/practica5/ejercicio4.hs
@@ -2,8 +2,12 @@
 {-# HLINT ignore "Use list literal pattern" #-}
 module Ejercicio4 where
 import Test.HUnit
+import Data.Bits (Bits(xor))
 
+lista1 :: String
 lista1 = "Ho    la m  u  n  d  o"
+lista2 :: String
+lista2 = "Esta  es una prueba     de haskell con    hola mundo"
 
 -- ? a) sacarBlancosRepetidos :: [Char] -> [Char]
 sacarBlancosRepetidos :: [Char] -> [Char]
@@ -12,16 +16,37 @@ sacarBlancosRepetidos (x:[]) = [x]
 sacarBlancosRepetidos (x:y:xs) | x == y && x == ' ' = sacarBlancosRepetidos (y:xs)
                                | otherwise = x : sacarBlancosRepetidos (y:xs)
 
+-- ? b) contarPalabras :: [Char] -> Integer,
+contarPalabras :: [Char] -> Integer
+contarPalabras [] = 0
+contarPalabras (x:[]) = 1
+contarPalabras (x:y:xs) | x == y && x == ' ' = contarPalabras (sacarVaciosRepetidos (x:y:xs))
+                        | x == ' ' = 1 + contarPalabras (y:xs)
+                        | otherwise = contarPalabras (y:xs)
+                        where sacarVaciosRepetidos [] = []
+                              sacarVaciosRepetidos (x:[]) = [x]
+                              sacarVaciosRepetidos (x:y:xs) | x == y && x == ' ' = sacarVaciosRepetidos(y:xs)
+                                                            | otherwise = x: sacarVaciosRepetidos(y:xs)
+
 -- ? TESTS
+-- ? b) contarPalabras
+testContarPalabras :: Test
+testContarPalabras = test [
+  "contar lista1" ~: contarPalabras lista1 ~=? 7,
+  "contar lista2" ~: contarPalabras lista2 ~=? 9
+ ]
+
 -- ? a) sacarBlancosRepetidos
 testSacarBlancosRepetidos :: Test
 testSacarBlancosRepetidos = test [
-  "sacar lista1" ~: sacarBlancosRepetidos lista1 ~=? "Ho la m u n d o"
+  "sacar lista1" ~: sacarBlancosRepetidos lista1 ~=? "Ho la m u n d o",
+  "sacar lista1" ~: sacarBlancosRepetidos lista2 ~=? "Esta es una prueba de haskell con hola mundo"
  ]
 
 tests = TestList [
-  TestLabel "testSacarBlancosRepetidos" testSacarBlancosRepetidos
+  TestLabel "testSacarBlancosRepetidos" testSacarBlancosRepetidos,
+  TestLabel "testContarPalabras" testContarPalabras
  ]
 
-corrertests :: IO Counts
-corrertests = runTestTT tests
+correrTests :: IO Counts
+correrTests = runTestTT tests
