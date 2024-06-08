d287c1eda2d473dd5d570d597028e0e2e3ab07a9
Alex Ballera <alexballera@gmail.com>
2024-06-07 21:52:04 -0300

    ejercicio 4.2

--- practicas/python/practica7/ejercicio4.py
+++ practicas/python/practica7/ejercicio4.py
@@ -9,4 +9,20 @@ def ingresar_nombres() -> list[str]:
   print(f'nombres: {nombres}')
   return nombres
 
-ingresar_nombres()
+# ingresar_nombres()
+
+# Ejercicio 2
+def historial_movimientos() -> list[(str, int)]:
+  tipo: str = ''
+  monto: int = 0
+  movimientos: list[(str, int)] = []
+  while (tipo != 'X'):
+    tipo = input('\nIngrese la letra del tipo de operación: \n\n "C" = Cargar crédito \n "D" = Descontar créditos \n "X" = Finalizar\n')
+    tipo = tipo.capitalize()
+    if (tipo != 'X'):
+      monto = int(input('Ingrese el monto: '))
+      movimientos.append((tipo, monto))
+  print(f'movimientos: {movimientos}')
+  return movimientos
+
+historial_movimientos()
