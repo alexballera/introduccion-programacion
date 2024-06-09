588507ba4c0826848a57cb163e03a509ac1ad707
Alex Ballera <alexander.ballera@comafi.com.ar>
2024-06-09 17:19:09 -0300

    Ej 7

--- practicas/python/practica8/archivos/ejercicio7.py
+++ practicas/python/practica8/archivos/ejercicio7.py
@@ -11,4 +11,10 @@ def promedio_estudiante(nombre_archivo, lu : str) -> float:
       notas += int(linea[3])
   
   promedio = notas / materias
+  estudiante = []
+  estudiante.append(lu, promedio)
+  nuevo_archivo = open('nombre_archivo_promedios.csv', 'w', encoding='utf-8')
+  nuevo_archivo.write(estudiante)
+  archivo.close()
+  nuevo_archivo.close()
   return promedio
