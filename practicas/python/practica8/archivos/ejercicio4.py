997468420e4564e76df73b38789f08d2d4992cda
Alex Ballera <alexander.ballera@comafi.com.ar>
2024-06-09 16:12:18 -0300

    add ejercicio5

--- practicas/python/practica8/archivos/ejercicio4.py
+++ practicas/python/practica8/archivos/ejercicio4.py
@@ -1,3 +1,4 @@
+# Ej 4
 def agregar_frase_al_final(nombre_archivo: str,frase : str):
   archivo = open(nombre_archivo, 'r', encoding='utf-8')
   archivo = archivo.readlines()
@@ -14,15 +15,17 @@ def agregar_frase_al_final(nombre_archivo: str,frase : str):
   
   for linea in archivo_nuevo:
     archivo.write(linea)
+  
+  archivo.close()
 
 # Mejorado con la documentación
 # https://recursospython.com/guias-y-manuales/lectura-y-escritura-de-archivos/
 
 def agregar_frase_al_final2(nombre_archivo: str,frase : str):
   archivo = open(nombre_archivo, '+a', encoding='utf-8')
-  archivo.write(frase)
+  archivo.write('\n' + frase)
   archivo.close()
 
 archivo = 'himno.txt'
-frase = '\nEsta es la frase agregada al final'
+frase = 'Esta es la frase agregada al final'
 agregar_frase_al_final2(archivo, frase)
---
+++ practicas/python/practica8/archivos/ejercicio5.py
@@ -0,0 +1,24 @@
+# Ej 5
+
+def agregar_frase_al_principio(nombre_archivo: str, frase : str):
+  archivo = open(nombre_archivo, 'r+', encoding='utf-8')
+  archivo_nuevo = archivo.readlines()
+  archivo_nuevo.insert(0, frase + '\n')
+  archivo.close()
+
+  archivo = open(nombre_archivo, 'w', encoding='utf-8')
+  archivo.truncate()
+  for linea in archivo_nuevo:
+    archivo.write(linea)
+
+def agregar_frase_al_principio2(nombre_archivo: str, frase : str):
+  archivo = open(nombre_archivo, 'r+', encoding='utf-8')
+  contenido = archivo.read()
+  archivo.seek(0)
+  archivo.write(frase + '\n' + contenido)
+  archivo.close()
+
+
+archivo = 'himno.txt'
+frase = 'Esta es la frase agregada al principio'
+agregar_frase_al_principio(archivo, frase)
