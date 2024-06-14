1634fcd621f245804ed77b5cf63ef86af1c0b324
Alex Ballera <alexballera@gmail.com>
2024-06-14 15:41:09 -0300

    colas

--- learn/tad.py
+++ learn/tad.py
@@ -24,3 +24,33 @@ while(not pila1.empty()):
 for item in lista3:
   pila3.put(item)
 print(f'lista3: {lista3}')
+
+print()
+
+# Colas
+import queue
+cola1 = queue.Queue()
+cola2 = queue.Queue()
+cola3 = queue.Queue()
+
+lista4 = ['a','e','i','o','u']
+lista5 = [1,2,3,4,5,6,7,8,9]
+lista6 = []
+
+for item in lista4:
+  cola1.put(item)
+print(f'primer cola1 {cola1.get()}')
+
+for i in range(len(lista5)):
+  cola2.put(lista5[i])
+print(f'primer cola2 {cola2.get()}')
+
+i = 0
+while (i < len(lista5)):
+  cola3.put(lista5[i])
+  i += 1
+print(f'primer cola3 {cola3.get()}')
+
+while (not cola3.empty()):
+  lista6.append(cola3.get())
+print(f'lista6 {lista6}')
