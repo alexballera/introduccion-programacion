c38b007240fa86a23b30816816a8301a279828a5
Alex Ballera <alexballera@gmail.com>
2024-06-14 22:57:36 -0300

    diccionario

--- learn/tad.py
+++ learn/tad.py
@@ -56,3 +56,54 @@ print(f'primer cola3 {cola3.get()}')
 while (not cola3.empty()):
   lista6.append(cola3.get())
 print(f'lista6 {lista6}')
+
+print()
+
+# Diccionario
+print('Diccionario')
+
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(f'tel {tel}')
+
+tel['jack']
+tel['irv'] = 4127
+print(f'tel {tel}')
+print(f'list(tel) = {list(tel)}')
+
+print()
+
+guido = 'guido' in tel
+print(f'guido in tel = {guido}')
+jack = 'jack' not in tel
+print(f'jack not in tel = {jack}')
+
+print()
+
+dictado1 = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print(f'dictado = {dictado1}')
+
+# Cuando las claves son cadenas simples, a veces resulta más fácil especificar los pares usando argumentos por palabra clave:
+dictado2 = dict(sape=4139, guido=4127, jack=4098)
+print(f'dictado2 = {dictado2}')
+
+print(f'list(dictado1)  = {list(dictado1)}')
+print(f'len(dictado1)  = {len(dictado1)}')
+print(f'dictado1["sape"]  = {dictado1["sape"]}')
+
+print()
+
+# key in d: Retorna True si d tiene una entrada en la clave key, False en caso contrario
+pertenece = 'sape' in dictado1
+print(f'"sape" in dictado1 {pertenece}')
+no_pertenece = not 'sape' in dictado1
+print(f'not "sape" in dictado1 {no_pertenece}')
+
+print()
+
+print('iteracion')
+# items() Retorna una nueva vista de los elementos del diccionario (pares (key, value))
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
+
